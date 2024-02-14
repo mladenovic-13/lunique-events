7ff6fee3301c371d8da7c2d8a2d0f80ef2935580
Nikola Mladenović <nikolanik999@gmail.com>
2024-02-14 13:53:57 +0100

    feat: images control bar

--- src/components/partials/event/edit-event-gallery.tsx
+++ src/components/partials/event/edit-event-gallery.tsx
@@ -7,6 +7,7 @@ import {
   ChevronRightIcon,
   ChevronsLeftIcon,
   ChevronsRightIcon,
+  TrashIcon,
 } from "lucide-react";
 import { useEffect, useState } from "react";
 import * as ToggleGroup from "@radix-ui/react-toggle-group";
@@ -16,6 +17,8 @@ import { cn } from "@/lib/utils";
 import { type Image as ImageProps } from "@prisma/client";
 import { api } from "@/trpc/react";
 import { OpenModalButton } from "@/components/buttons/open-modal-button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Switch } from "@/components/ui/switch";
 
 interface EditEventGalleryProps {
   eventId: string;
@@ -25,6 +28,7 @@ export const EditEventGallery = ({ eventId }: EditEventGalleryProps) => {
   const [selected, setSelected] = useState<string[]>([]);
   const [selectedImages, setSelectedImages] = useState<ImageProps[]>([]);
   const [images, setImages] = useState<ImageProps[]>([]);
+  const [isSelectMode, setIsSelectMode] = useState(false);
 
   const [page, setPage] = useState<number>(0);
 
@@ -52,6 +56,8 @@ export const EditEventGallery = ({ eventId }: EditEventGalleryProps) => {
       page.images.forEach((image) => tmpImages.push(image));
     });
 
+    console.log({ data });
+
     setImages(tmpImages);
   }, [data]);
 
@@ -72,23 +78,76 @@ export const EditEventGallery = ({ eventId }: EditEventGalleryProps) => {
     }
   };
 
+  const handleSelectAll = () => {
+    setSelected(() => images.map((img) => img.key));
+    setSelectedImages(images);
+  };
+
+  const handleDesectAll = () => {
+    setSelected([]);
+    setSelectedImages([]);
+  };
+
   return (
     <div className="space-y-3">
-      <div className="flex items-center justify-between">
-        <p className="text-zinc-500">
-          {selected.length} of {imagesCount} selected
-        </p>
-        <OpenModalButton
-          modalType="delete-event-images"
-          modalData={{ galleryImages: selectedImages }}
-          variant="destructive"
-          disabled={selected.length === 0}
-        >
-          Delete
-        </OpenModalButton>
+      <div className="flex h-8 w-full items-center rounded-lg bg-primary">
+        <div className="flex h-full w-full items-center">
+          <div className="flex h-full items-center border-r-2 border-primary-foreground px-3">
+            <Checkbox
+              disabled={!isSelectMode}
+              className="border-primary-foreground data-[checked=true]:opacity-50"
+              onCheckedChange={(checked) => {
+                if (checked) handleSelectAll();
+                else handleDesectAll();
+              }}
+            />
+          </div>
+          <div className="flex w-full items-center justify-between gap-3 pl-3">
+            <p className="text-xs text-primary-foreground">
+              {selected.length} of {imagesCount} selected
+            </p>
+            <div className="flex items-center gap-3">
+              <div className="flex items-center gap-1.5">
+                <p
+                  className={cn(
+                    "text-xs font-bold",
+                    isSelectMode
+                      ? "text-primary-foreground/40"
+                      : "text-primary-foreground",
+                  )}
+                >
+                  PREVIEW
+                </p>
+                <Switch
+                  checked={isSelectMode}
+                  onCheckedChange={setIsSelectMode}
+                />
+                <p
+                  className={cn(
+                    "text-xs font-bold",
+                    isSelectMode
+                      ? "text-primary-foreground"
+                      : "text-primary-foreground/40",
+                  )}
+                >
+                  SELECT
+                </p>
+                <OpenModalButton
+                  disabled={!isSelectMode || selected.length === 0}
+                  className="h-8 px-3 text-white data-[disabled=true]:opacity-50"
+                  modalType="delete-event-images"
+                  modalData={{ galleryImages: selectedImages }}
+                >
+                  <TrashIcon className="h-5 w-5 " />
+                </OpenModalButton>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="flex items-center gap-3 md:flex-col md:items-start md:gap-1.5"></div>
       <ToggleGroup.Root
+        disabled={!isSelectMode}
         type="multiple"
         orientation="horizontal"
         value={selected}
@@ -99,7 +158,10 @@ export const EditEventGallery = ({ eventId }: EditEventGalleryProps) => {
           <ToggleGroup.Item
             key={image.id}
             value={image.key}
-            className="relative rounded-lg p-0.5 data-[state=on]:ring-2"
+            className={cn(
+              "relative rounded-lg p-0.5 data-[state=on]:ring-2",
+              !isSelectMode && "data-[state=on]:ring-0",
+            )}
           >
             <AspectRatio ratio={1 / 1}>
               <Image
@@ -111,19 +173,21 @@ export const EditEventGallery = ({ eventId }: EditEventGalleryProps) => {
               />
             </AspectRatio>
 
-            <div
-              className={cn(
-                "absolute left-3 top-3 flex h-4 w-4 items-center justify-center rounded-sm border border-primary",
-                selected.includes(image.key) && "bg-primary shadow-white",
-              )}
-            >
-              <CheckIcon
+            {isSelectMode && (
+              <div
                 className={cn(
-                  "hidden text-primary-foreground",
-                  selected.includes(image.key) && "block",
+                  "absolute left-3 top-3 flex h-4 w-4 items-center justify-center rounded-sm border border-primary",
+                  selected.includes(image.key) && "bg-primary shadow-white",
                 )}
-              />
-            </div>
+              >
+                <CheckIcon
+                  className={cn(
+                    "hidden text-primary-foreground",
+                    selected.includes(image.key) && "block",
+                  )}
+                />
+              </div>
+            )}
           </ToggleGroup.Item>
         ))}
       </ToggleGroup.Root>
