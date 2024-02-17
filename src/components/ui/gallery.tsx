4c2a8d975f1f1a353a3e6c5ec96ffcd751aaf62e
Nikola Mladenović <nikolanik999@gmail.com>
2024-02-17 19:41:12 +0100

    fix: replace gallery modal

--- src/components/ui/gallery.tsx
+++ src/components/ui/gallery.tsx
@@ -5,39 +5,9 @@ import useEmblaCarousel, {
   type UseEmblaCarouselType,
 } from "embla-carousel-react";
 import Image from "next/image";
-
-import "@/styles/gallery.css";
-
-import image1 from "@/public/images/1.webp";
-import image2 from "@/public/images/2.webp";
-import image3 from "@/public/images/3.webp";
-import image4 from "@/public/images/4.webp";
-import image5 from "@/public/images/5.webp";
+import { type Image as ImageType } from "@prisma/client";
 import { ChevronLeft, ChevronRight, Download, Share, X } from "lucide-react";
 import { cn } from "@/lib/utils";
-
-export const images = [
-  image1.src,
-  image2.src,
-  image3.src,
-  image4.src,
-  image5.src,
-  image1.src,
-  image2.src,
-  image3.src,
-  image4.src,
-  image5.src,
-  image1.src,
-  image2.src,
-  image3.src,
-  image4.src,
-  image5.src,
-  image1.src,
-  image2.src,
-  image3.src,
-  image4.src,
-  image5.src,
-];
 
 export type GalleryOptions = {
   thumbs?: boolean;
@@ -52,9 +22,15 @@ export type GalleryHandlers = {
   onShare?: () => void;
 };
 
-type GalleryProps = GalleryOptions & GalleryHandlers;
+type GalleryProps = {
+  images: ImageType[];
+  currentImage: number;
+} & GalleryOptions &
+  GalleryHandlers;
 
 export const Gallery = ({
+  images,
+  currentImage = 0,
   thumbs,
   chevrons = true,
   close,
@@ -83,13 +59,23 @@ export const Gallery = ({
 
   const onSelect = useCallback(() => {
     if (!mainCarouselApi || !thumbCarouselApi) return;
+
     setSelectedIndex(mainCarouselApi.selectedScrollSnap());
     thumbCarouselApi.scrollTo(mainCarouselApi.selectedScrollSnap());
   }, [mainCarouselApi, thumbCarouselApi, setSelectedIndex]);
 
   useEffect(() => {
+    if (!mainCarouselApi || !thumbCarouselApi || !currentImage) return;
+
+    mainCarouselApi.scrollTo(currentImage, true);
+    thumbCarouselApi.scrollTo(currentImage);
+  }, [currentImage, mainCarouselApi, thumbCarouselApi]);
+
+  useEffect(() => {
     if (!mainCarouselApi) return;
+
     onSelect();
+
     mainCarouselApi.on("select", onSelect);
     mainCarouselApi.on("reInit", onSelect);
   }, [mainCarouselApi, onSelect]);
@@ -113,7 +99,6 @@ export const Gallery = ({
 
   return (
     <div className="relative bg-primary">
-      {/* CONTROLS */}
       <div className="absolute right-3 top-3 z-10 flex gap-3 md:right-5 md:top-5 md:gap-5">
         {download && (
           <button
@@ -144,24 +129,28 @@ export const Gallery = ({
 
       {chevrons && (
         <>
-          <button
-            className="group absolute left-0 top-[calc(50%-256px)] z-20 flex h-64 w-12 translate-y-[50%] items-center justify-center transition duration-200 hover:bg-white/5 md:left-5 md:w-20"
-            onClick={handleLeft}
-          >
-            <ChevronLeft className="h-6 w-6 bg-clip-content text-primary-foreground transition duration-200 group-hover:scale-110 md:h-10 md:w-10" />
-          </button>
-          <button
-            className="group absolute right-0 top-[calc(50%-256px)] z-20 flex h-64 w-12 translate-y-[50%] items-center justify-center transition duration-200 hover:bg-white/5 md:right-5 md:w-20"
-            onClick={handleRight}
-          >
-            <ChevronRight className="h-6 w-6 bg-clip-content text-primary-foreground transition duration-200 group-hover:scale-110 md:h-10 md:w-10" />
-          </button>
+          {mainCarouselApi?.canScrollPrev() && (
+            <button
+              className="group absolute left-0 top-[calc(50%-256px)] z-20 flex h-64 w-12 translate-y-[50%] items-center justify-center transition duration-200 hover:bg-white/5 md:left-5 md:w-20"
+              onClick={handleLeft}
+            >
+              <ChevronLeft className="h-6 w-6 bg-clip-content text-primary-foreground transition duration-200 group-hover:scale-110 md:h-10 md:w-10" />
+            </button>
+          )}
+          {mainCarouselApi?.canScrollNext() && (
+            <button
+              className="group absolute right-0 top-[calc(50%-256px)] z-20 flex h-64 w-12 translate-y-[50%] items-center justify-center transition duration-200 hover:bg-white/5 md:right-5 md:w-20"
+              onClick={handleRight}
+            >
+              <ChevronRight className="h-6 w-6 bg-clip-content text-primary-foreground transition duration-200 group-hover:scale-110 md:h-10 md:w-10" />
+            </button>
+          )}
         </>
       )}
 
       <div className="overflow-hidden" ref={mainCarouselRef}>
         <div className="flex touch-pan-y gap-1.5">
-          {images.map((src, idx) => (
+          {images.map((img, idx) => (
             <div
               style={{
                 flex: "0 0 100%",
@@ -170,14 +159,14 @@ export const Gallery = ({
               key={idx}
             >
               <Image
-                src={src}
+                src={img.url}
                 fill
                 className="absolute object-cover blur-2xl brightness-75"
                 alt=""
               />
               <div className="flex h-full w-full items-center justify-center">
                 <div className="relative h-[500px] w-full lg:h-[800px]">
-                  <Image src={src} fill alt="" className="object-contain" />
+                  <Image src={img.url} fill alt="" className="object-contain" />
                 </div>
                 I
               </div>
@@ -189,6 +178,7 @@ export const Gallery = ({
       {thumbs && (
         <div className="absolute bottom-3 z-10 flex w-full items-center justify-center">
           <Thumbs
+            images={images}
             carouselRef={thumbCarouselRef}
             onThumbClick={onThumbClick}
             selectedIndex={selectedIndex}
@@ -200,22 +190,28 @@ export const Gallery = ({
 };
 
 type ThumbsProps = {
+  images: ImageType[];
   onThumbClick: (index: number) => void;
   carouselRef: UseEmblaCarouselType["0"];
   selectedIndex: number;
 };
 
-const Thumbs = ({ carouselRef, onThumbClick, selectedIndex }: ThumbsProps) => {
+const Thumbs = ({
+  images,
+  carouselRef,
+  onThumbClick,
+  selectedIndex,
+}: ThumbsProps) => {
   return (
     <div className="overflow-hidden" ref={carouselRef}>
       <div className="flex gap-0.5 md:gap-0">
-        {images.map((src, idx) => (
+        {images.map((img, idx) => (
           <ThumbButton
             key={idx}
             index={idx}
             selected={idx === selectedIndex}
             onClick={() => onThumbClick(idx)}
-            src={src}
+            src={img.url}
           />
         ))}
       </div>
@@ -234,16 +230,16 @@ const ThumbButton = ({ index, selected, src, onClick }: ThumbButtonProps) => (
   <button
     onClick={() => onClick(index)}
     className={cn(
-      "w-1/5 flex-none flex-shrink-0 flex-grow-0 rounded-sm transition duration-200 md:w-[8%]",
+      "h-12 w-1/5 flex-none flex-shrink-0 flex-grow-0 rounded-sm transition duration-200 md:h-16 md:w-[8%]",
       !selected && "scale-90 opacity-25 hover:opacity-50",
     )}
   >
     <Image
+      className="rounded-sm object-cover"
       src={src}
       width={120}
       height={60}
       alt=""
-      className="rounded-sm object-contain"
     />
   </button>
 );
