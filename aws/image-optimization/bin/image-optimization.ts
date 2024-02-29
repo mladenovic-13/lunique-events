826f867d379e518317423a92c66c4625fa4a9f47
Nikola Mladenović <nikolanik999@gmail.com>
2024-02-29 16:26:13 +0100

    remove cloud formation stack code

--- aws/image-optimization/bin/image-optimization.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { ImageOptimizationStack } from "../lib/image-optimization-stack";
-
-const app = new cdk.App();
-new ImageOptimizationStack(app, "ImageOptimizationStack", {});
