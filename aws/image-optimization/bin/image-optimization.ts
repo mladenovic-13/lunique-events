3f7d298ca98933b2d36b20224258a4e33d37834e
Nikola Mladenović <nikolanik999@gmail.com>
2024-02-29 16:01:33 +0100

    aws cloud formation stack for image optimization

---
+++ aws/image-optimization/bin/image-optimization.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { ImageOptimizationStack } from "../lib/image-optimization-stack";
+
+const app = new cdk.App();
+new ImageOptimizationStack(app, "ImageOptimizationStack", {});
