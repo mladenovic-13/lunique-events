dfbe12c23b3f8acbe6c471c6948cef6e73f2e418
Nikola Mladenović <nikolanik999@gmail.com>
2024-03-07 17:00:41 +0100

    fix: remove plans page

--- src/app/(billing)/plans/page.tsx
+++ src/app/(billing)/plans/page.tsx
@@ -1,6 +1,6 @@
-import { CheckoutButton } from "@/components/partials/billing/checkout-button";
+// import { CheckoutButton } from "@/components/partials/billing/checkout-button";
 import { api } from "@/trpc/server";
-import { type Plan } from "@prisma/client";
+// import { type Plan } from "@prisma/client";
 
 export default async function Plans() {
   const plans = await api.billing.getAllPlans.query();
@@ -12,39 +12,39 @@ export default async function Plans() {
   return (
     <div>
       <h2>Plans</h2>
-
+      {/* 
       <div className="mb-5 mt-3 grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-5">
         {plans.map((plan, index) => {
           return <Plan key={`plan-${index}`} plan={plan} />;
         })}
-      </div>
+      </div> */}
     </div>
   );
 }
 
-export function Plan({ plan }: { plan: Plan }) {
-  const { description, productName, name, price } = plan;
+// export function Plan({ plan }: { plan: Plan }) {
+//   const { description, productName, name, price } = plan;
 
-  return (
-    <div>
-      <h2>
-        {productName} ({name})
-      </h2>
+//   return (
+//     <div>
+//       <h2>
+//         {productName} ({name})
+//       </h2>
 
-      {description ? (
-        <div
-          dangerouslySetInnerHTML={{
-            // Ideally sanitize the description first.
-            __html: description,
-          }}
-        ></div>
-      ) : null}
+//       {description ? (
+//         <div
+//           dangerouslySetInnerHTML={{
+//             // Ideally sanitize the description first.
+//             __html: description,
+//           }}
+//         ></div>
+//       ) : null}
 
-      <p>${price}</p>
+//       <p>${price}</p>
 
-      <CheckoutButton plan={plan} embed={true}>
-        Get Plan
-      </CheckoutButton>
-    </div>
-  );
-}
+//       <CheckoutButton plan={plan} embed={true}>
+//         Get Plan
+//       </CheckoutButton>
+//     </div>
+//   );
+// }
