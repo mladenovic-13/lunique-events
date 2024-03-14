0e95f7a01f6080806b805ae7b84cc19b5b7024b9
Lukiano99 <l.stojadinovic99@gmail.com>
2024-03-14 16:52:37 +0100

    Responsive design for big screen

--- src/app/(admin)/events/create/page.tsx
+++ src/app/(admin)/events/create/page.tsx
@@ -31,8 +31,8 @@ export default function CreateEventPage() {
   const { onOpen } = useModal();
 
   return (
-    <div className="mx-auto max-w-4xl space-y-5 p-3 pb-10">
-      <div className="w-full space-y-5 px-10">
+    <div className="mx-auto max-w-4xl space-y-5 p-3 pb-10 md:flex  md:space-x-8 md:space-y-0 ">
+      <div className="w-full space-y-5 px-10 md:flex-1 md:px-0">
         <div className="relative">
           <AspectRatio ratio={1 / 1}>
             <Image
@@ -63,121 +63,125 @@ export default function CreateEventPage() {
         </button>
       </div>
 
-      <div className="flex justify-between">
-        <CalendarSelect />
-        <VisibilitySelect />
-      </div>
+      <div className="space-y-5 md:w-3/5">
+        <div className="flex justify-between">
+          <CalendarSelect />
+          <VisibilitySelect />
+        </div>
 
-      <Input
-        className="h-12 border-none text-4xl font-medium focus-visible:ring-0"
-        placeholder="Event name"
-      />
+        <Input
+          className="h-12 border-none text-4xl font-medium focus-visible:ring-0"
+          placeholder="Event name"
+        />
 
-      <div className="flex gap-2">
-        <div className="flex-1 rounded-md bg-muted p-1">
-          <div className="flex h-full gap-8">
-            <div className="flex">
-              <div className="flex h-full flex-col items-center p-3 py-2.5">
-                <CircleIcon className="h-3 w-3 rounded-full bg-muted-foreground/80 text-muted-foreground/80" />
-                <div className="flex-1 border-r border-dashed border-muted-foreground/50" />
-                <CircleIcon className="h-3 w-3 text-muted-foreground/80" />
+        <div className="flex gap-2">
+          <div className="flex-1 rounded-md bg-muted p-1">
+            <div className="flex h-full gap-8">
+              <div className="flex">
+                <div className="flex h-full flex-col items-center p-3 py-2.5">
+                  <CircleIcon className="h-3 w-3 rounded-full bg-muted-foreground/80 text-muted-foreground/80" />
+                  <div className="flex-1 border-r border-dashed border-muted-foreground/50" />
+                  <CircleIcon className="h-3 w-3 text-muted-foreground/80" />
+                </div>
+                <div className="flex flex-col justify-between py-[5px] font-light">
+                  <span>Start</span>
+                  <span>End</span>
+                </div>
               </div>
-              <div className="flex flex-col justify-between py-[5px] font-light">
-                <span>Start</span>
-                <span>End</span>
+              <div className="flex h-full flex-1 flex-col gap-1">
+                <div className="flex h-full justify-between rounded-md bg-muted-foreground/20 font-light">
+                  <span className="px-2 py-1">Wed 6, Mar</span>
+                  <span className="w-16 border-l-2 border-muted py-1 text-center ">
+                    11:30
+                  </span>
+                </div>
+                <div className="flex h-full justify-between rounded-md bg-muted-foreground/20 font-light">
+                  <span className="px-2 py-1">Wed 20, Mar</span>
+                  <span className="w-16 border-l-2 border-muted py-1 text-center ">
+                    12:30
+                  </span>
+                </div>
               </div>
             </div>
-            <div className="flex h-full flex-1 flex-col gap-1">
-              <div className="flex h-full justify-between rounded-md bg-muted-foreground/20 font-light">
-                <span className="px-2 py-1">Wed 6, Mar</span>
-                <span className="w-16 border-l-2 border-muted py-1 text-center ">
-                  11:30
-                </span>
-              </div>
-              <div className="flex h-full justify-between rounded-md bg-muted-foreground/20 font-light">
-                <span className="px-2 py-1">Wed 20, Mar</span>
-                <span className="w-16 border-l-2 border-muted py-1 text-center ">
-                  12:30
-                </span>
-              </div>
+          </div>
+
+          <div className="flex w-36 flex-col gap-1.5 rounded-md bg-muted px-3 py-1.5">
+            <GlobeIcon className="h-5 w-5 text-muted-foreground" />
+            <div className="flex flex-col">
+              <span className="text-sm">GMT+01:00</span>
+              <span className="text-xs text-muted-foreground">Belgrade</span>
             </div>
           </div>
         </div>
 
-        <div className="flex w-36 flex-col gap-1.5 rounded-md bg-muted px-3 py-1.5">
-          <GlobeIcon className="h-5 w-5 text-muted-foreground" />
-          <div className="flex flex-col">
-            <span className="text-sm">GMT+01:00</span>
-            <span className="text-xs text-muted-foreground">Belgrade</span>
+        <div>
+          <button className="w-full rounded-md border border-border bg-muted px-3.5 py-1.5 transition duration-300 hover:border-muted-foreground/60 hover:bg-muted-foreground/30">
+            <span className="flex flex-col items-start">
+              <span className="flex items-center gap-3">
+                <MapPinIcon className="h-4 w-4 text-muted-foreground" />
+                <span className="text-lg font-medium">Add Event Location</span>
+              </span>
+              <span className="pl-7 text-sm text-muted-foreground">
+                Offline location or virtual link
+              </span>
+            </span>
+          </button>
+        </div>
+
+        <div>
+          <button className="flex w-full items-center gap-3 rounded-md border border-border bg-muted px-3.5 py-1.5 transition duration-300 hover:border-muted-foreground/60 hover:bg-muted-foreground/30">
+            <NewspaperIcon className="h-4 w-4 text-muted-foreground" />
+            <span className="text-lg font-medium">Add Description</span>
+          </button>
+        </div>
+
+        <div className="space-y-2">
+          <p className="text-sm font-semibold text-muted-foreground">
+            Event Options
+          </p>
+          <div className="rounded-md bg-muted ">
+            <EventOptionButton
+              title="Tickets"
+              activeColorClass="text-[#47C87F]"
+              isActive={false}
+              Icon={TicketIcon}
+              onClick={() => alert("Tickets clicked")}
+              slot={
+                <span className="flex items-center gap-1.5">
+                  <span className="text-muted-foreground">Free</span>
+                  <Pencil className="h-4 w-4 text-muted-foreground" />
+                </span>
+              }
+            />
+            <EventOptionButton
+              title="Require approval"
+              Icon={UserCheckIcon}
+              isActive={true}
+              activeColorClass="text-[#D77F48]"
+              slot={
+                <Switch className="focus-visible:ring-0 data-[state=checked]:bg-blue-500 data-[state=unchecked]:bg-muted-foreground" />
+              }
+            />
+            <EventOptionButton
+              title="Capacity"
+              Icon={ArrowUpToLineIcon}
+              isActive={true}
+              activeColorClass="text-[#B596FF]"
+              onClick={() => alert("Tickets clicked")}
+              slot={
+                <span className="flex items-center gap-1.5">
+                  <span className="text-muted-foreground">Unlimited</span>
+                  <Pencil className="h-4 w-4 text-muted-foreground" />
+                </span>
+              }
+            />
           </div>
         </div>
-      </div>
 
-      <div>
-        <button className="w-full rounded-md border border-border bg-muted px-3.5 py-1.5 transition duration-300 hover:border-muted-foreground/60 hover:bg-muted-foreground/30">
-          <span className="flex flex-col items-start">
-            <span className="flex items-center gap-3">
-              <MapPinIcon className="h-4 w-4 text-muted-foreground" />
-              <span className="text-lg font-medium">Add Event Location</span>
-            </span>
-            <span className="pl-7 text-sm text-muted-foreground">
-              Offline location or virtual link
-            </span>
-          </span>
-        </button>
+        <Button size="lg" className="w-full">
+          Create Event
+        </Button>
       </div>
-
-      <div>
-        <button className="flex w-full items-center gap-3 rounded-md border border-border bg-muted px-3.5 py-1.5 transition duration-300 hover:border-muted-foreground/60 hover:bg-muted-foreground/30">
-          <NewspaperIcon className="h-4 w-4 text-muted-foreground" />
-          <span className="text-lg font-medium">Add Description</span>
-        </button>
-      </div>
-
-      <div>
-        <p className="text-sm font-semibold text-muted-foreground">
-          Event Options
-        </p>
-        <div className="rounded-md bg-muted ">
-          <EventOptionButton
-            title="Tickets"
-            activeColorClass="text-[#47C87F]"
-            isActive={false}
-            Icon={TicketIcon}
-            onClick={() => alert("Tickets clicked")}
-            slot={
-              <span className="flex items-center gap-1.5">
-                <span className="text-muted-foreground">Free</span>
-                <Pencil className="h-4 w-4 text-muted-foreground" />
-              </span>
-            }
-          />
-          <EventOptionButton
-            title="Require approval"
-            Icon={UserCheckIcon}
-            isActive={true}
-            activeColorClass="text-[#D77F48]"
-            slot={<Switch />}
-          />
-          <EventOptionButton
-            title="Capacity"
-            Icon={ArrowUpToLineIcon}
-            isActive={true}
-            activeColorClass="text-[#B596FF]"
-            onClick={() => alert("Tickets clicked")}
-            slot={
-              <span className="flex items-center gap-1.5">
-                <span className="text-muted-foreground">Unlimited</span>
-                <Pencil className="text-muted-foregrounde h-4 w-4" />
-              </span>
-            }
-          />
-        </div>
-      </div>
-
-      <Button size="lg" className="w-full">
-        Create Event
-      </Button>
     </div>
   );
 }
