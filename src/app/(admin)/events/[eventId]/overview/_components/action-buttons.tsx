063e17eb322f4c8cddaf7a3b9fceb858fc274147
Nikola Mladenović <nikolanik999@gmail.com>
2024-03-14 19:42:22 +0100

    chore: details, guests design

--- src/app/(admin)/events/[eventId]/overview/_components/action-buttons.tsx
+++ src/app/(admin)/events/[eventId]/overview/_components/action-buttons.tsx
@@ -1,11 +1,15 @@
 "use client";
 
-import { MailOpenIcon, MessageSquareIcon, ShareIcon } from "lucide-react";
-import { ActionButton } from "./action-button";
+import {
+  type LucideIcon,
+  MailOpenIcon,
+  MessageSquareIcon,
+  ShareIcon,
+} from "lucide-react";
 
 export const ActionButtons = () => {
   return (
-    <div className="flex gap-1.5 overflow-x-auto pb-1.5 md:overflow-hidden">
+    <div className="flex gap-1.5 overflow-x-auto pb-1.5 md:grid md:grid-cols-3 md:overflow-hidden">
       <ActionButton
         title="Invite Guests"
         Icon={MailOpenIcon}
@@ -22,5 +26,24 @@ export const ActionButtons = () => {
         onClick={() => alert("Share Event")}
       />
     </div>
+  );
+};
+
+const ActionButton = (props: {
+  title: string;
+  Icon: LucideIcon;
+  onClick: () => void;
+}) => {
+  const { title, Icon, onClick } = props;
+  return (
+    <button
+      onClick={onClick}
+      className="flex min-w-fit items-center gap-3 rounded-md bg-muted p-1.5 pr-16 text-sm font-medium"
+    >
+      <div className="h-full rounded-md bg-muted-foreground/10 p-1.5">
+        <Icon className="h-6 w-6" />
+      </div>
+      {title}
+    </button>
   );
 };
--- src/app/(admin)/events/[eventId]/overview/page.tsx
+++ src/app/(admin)/events/[eventId]/overview/page.tsx
@@ -1,4 +1,11 @@
 import { ActionButtons } from "./_components/action-buttons";
+import { EventPagePreview } from "./_components/event-page-preview";
+import { SocialButtons } from "./_components/social-buttons";
+import { EventDetails } from "./_components/event-details";
+import { CheckInButton } from "./_components/check-in-button";
+import { EditButtons } from "./_components/edit-buttons";
+import { InviteInsights } from "./_components/invite-insights";
+import { GuestList } from "./_components/guest-list";
 
 export default function EventOverviewPage({} // params: { eventId },
 : {
@@ -7,21 +14,40 @@ export default function EventOverviewPage({} // params: { eventId },
   };
 }) {
   return (
-    <div>
+    <div className="space-y-3 md:space-y-5">
       <ActionButtons />
 
-      <div>
-        <div></div>
-        <div>
-          <div>
-            <h2>When & Where</h2>
-            <div>date</div>
-            <div>location</div>
-            <p>This address is shown publicly on the event page.</p>
+      <div className="space-y-3 rounded-md bg-muted p-3 md:grid md:grid-cols-2 md:gap-5 md:p-5">
+        <div className="space-y-3 md:flex md:flex-col md:justify-between">
+          <EventPagePreview />
+          <SocialButtons />
+        </div>
+
+        <div className="space-y-3 md:flex md:flex-col md:justify-between">
+          <div className="space-y-3 md:space-y-5">
+            <EventDetails />
+            <CheckInButton />
           </div>
-          <div>
-            <button>Edit Event</button>
-            <button>Change Photo</button>
+
+          <EditButtons />
+        </div>
+      </div>
+
+      <div className="space-y-3 md:space-y-5">
+        <div className="flex flex-col">
+          <h2 className="text-2xl font-semibold">Invites</h2>
+          <p className=" text-muted-foreground">
+            Invite subscibers, contacts and past guests via email or SMS
+          </p>
+        </div>
+
+        <div className="grid  grid-cols-1 gap-3 md:grid-cols-3 md:gap-5">
+          <div className="md:col-span-1">
+            <InviteInsights />
+          </div>
+
+          <div className="md:col-span-2">
+            <GuestList />
           </div>
         </div>
       </div>
