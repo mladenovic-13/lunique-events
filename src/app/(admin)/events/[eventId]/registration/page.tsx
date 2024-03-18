5c7be81a511bd898ae0e81065998dbd0a81d551b
Lukiano99 <l.stojadinovic99@gmail.com>
2024-03-18 13:01:50 +0100

    feat: registration page, progress

--- src/app/(admin)/events/[eventId]/registration/page.tsx
+++ src/app/(admin)/events/[eventId]/registration/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { ArrowUpToLineIcon, Ticket, TicketIcon } from "lucide-react";
+
+import { ActionButton } from "../overview/_components/action-buttons";
+
 export default function RegistrationPage({
   params,
 }: {
@@ -5,5 +11,26 @@ export default function RegistrationPage({
     eventId: string;
   };
 }) {
-  return <div>TODO: {params.eventId} registration</div>;
+  return (
+    <div className="flex flex-col space-y-4">
+      <div>TODO: {params.eventId} registration</div>
+      <div className="flex gap-1.5 overflow-x-auto pb-1.5 md:grid md:grid-cols-3 md:overflow-hidden">
+        <ActionButton
+          title="Registration"
+          Icon={Ticket}
+          onClick={() => alert("Registration")}
+        />
+        <ActionButton
+          title="Event Capacity"
+          Icon={ArrowUpToLineIcon}
+          onClick={() => alert("Event Capacity")}
+        />
+        <ActionButton
+          title="Group Registration"
+          Icon={TicketIcon}
+          onClick={() => alert("Group Registration")}
+        />
+      </div>
+    </div>
+  );
 }
