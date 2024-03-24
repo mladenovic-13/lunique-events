3f41db1b129dee5034d64f44d4a2144876b406d5
Nikola Mladenović <nikolanik999@gmail.com>
2024-03-24 17:20:09 +0100

    feat: init design

--- src/app/(client)/(event)/[eventId]/(root)/page.tsx
+++ src/app/(client)/(event)/[eventId]/(root)/page.tsx
@@ -1,9 +1,39 @@
-export default function ClientEventIdPage({
-  params: { eventId },
-}: {
-  params: {
-    eventId: string;
-  };
-}) {
-  return <div>{eventId} page</div>;
+import { EventContact } from "./_components/event-contact";
+import { EventDetails } from "./_components/event-details";
+import { EventGuests } from "./_components/event-guests";
+import { EventHostedBy } from "./_components/event-hosted-by";
+import { EventLocation } from "./_components/event-location";
+import { EventThumbnail } from "./_components/event-thumbnail";
+import { RegisterGuest } from "./_components/register-guest";
+export default function ClientEventIdPage(
+  {
+    // params: { eventId },
+  }: {
+    params: {
+      eventId: string;
+    };
+  },
+) {
+  return (
+    <div className="mx-auto max-w-4xl space-y-5 p-3 pb-10 md:flex md:gap-5">
+      <div className="space-y-5 md:w-2/5">
+        <EventThumbnail />
+        <div className="hidden space-y-5 md:block">
+          <EventHostedBy />
+          <EventGuests />
+          <EventContact />
+        </div>
+      </div>
+      <div className="space-y-5 md:w-3/5">
+        <EventDetails />
+        <RegisterGuest />
+        <EventLocation />
+        <div className="space-y-5 md:hidden">
+          <EventHostedBy />
+          <EventGuests />
+          <EventContact />
+        </div>
+      </div>
+    </div>
+  );
 }
