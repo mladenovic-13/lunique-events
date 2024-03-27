c7334e884c4459fd047286c0d5bae19d14fcafcb
Lukiano99 <l.stojadinovic99@gmail.com>
2024-03-27 13:17:04 +0100

    feat: create event skeleton

---
+++ src/app/event/create/loading.tsx
@@ -0,0 +1,5 @@
+import { EventSettingsSkeleton } from "../manage/[eventId]/settings/_components/event-settings-skeleton";
+
+export default function Loading() {
+  return <EventSettingsSkeleton />;
+}
