855c3d88367afa17085ecdbd9bb0bf5adcf0d3b2
Lukiano99 <l.stojadinovic99@gmail.com>
2024-03-27 15:09:31 +0100

    fix: red events logo added

--- src/components/icons/themed-logo-icon.tsx
+++ src/components/icons/themed-logo-icon.tsx
@@ -4,15 +4,24 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 
-import darkLogo from "@/public/images/logo-dark.webp";
-import lightLogo from "@/public/images/logo-light.webp";
+// import darkLogo from "@/public/images/logo-dark.webp";
+// import lightLogo from "@/public/images/logo-light.webp";
+import redDarkLogo from "@/public/images/red-dark-events.webp";
+import redLightLogo from "@/public/images/red-light-events.webp";
 
 export const ThemedLogoIcon = () => {
   const { resolvedTheme } = useTheme();
-  const [src, setSrc] = useState(lightLogo.src);
+  // const [src, setSrc] = useState(lightLogo.src);
+
+  // useEffect(() => {
+  //   const logoSrc = resolvedTheme === "light" ? darkLogo.src : lightLogo.src;
+  //   setSrc(logoSrc);
+  // }, [resolvedTheme]);
+  const [src, setSrc] = useState(redLightLogo.src);
 
   useEffect(() => {
-    const logoSrc = resolvedTheme === "light" ? darkLogo.src : lightLogo.src;
+    const logoSrc =
+      resolvedTheme === "light" ? redDarkLogo.src : redLightLogo.src;
     setSrc(logoSrc);
   }, [resolvedTheme]);
 
