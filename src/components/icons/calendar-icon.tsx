5b94caaa617af84444719177a2016f8e69ebdd6b
Nikola Mladenović <72360450+mladenovic-13@users.noreply.github.com>
2024-03-31 17:46:46 +0200

    feat: user profile page (#91)

--- src/components/icons/calendar-icon.tsx
+++ src/components/icons/calendar-icon.tsx
@@ -14,12 +14,23 @@ export const CalendarIcon = ({
       "rounded-md border",
       size === "sm" && "size-8",
       size === "base" && "size-11 ",
+      size === "lg" && "size-14",
     )}
   >
-    <div className="flex h-2/5 items-center justify-center rounded-t-md bg-muted text-[10px] font-medium uppercase tracking-widest text-muted-foreground">
+    <div
+      className={cn(
+        "flex h-2/5 items-center justify-center rounded-t-md bg-muted text-[10px] font-medium uppercase tracking-widest text-muted-foreground",
+        size === "lg" && "text-xs",
+      )}
+    >
       {format(date, "MMM")}
     </div>
-    <div className="flex h-3/5 items-center justify-center text-sm font-medium">
+    <div
+      className={cn(
+        "flex h-3/5 items-center justify-center text-sm font-medium",
+        size === "lg" && "text-lg",
+      )}
+    >
       {date.getDate()}
     </div>
   </div>
