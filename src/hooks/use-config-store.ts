88168ebac3087ae2edd472085c8a56ac87831f14
Nikola Mladenović <72360450+mladenovic-13@users.noreply.github.com>
2024-04-15 15:55:31 +0200

    fix: sync latest changes (#112)

--- src/hooks/use-config-store.ts
+++ src/hooks/use-config-store.ts
@@ -2,25 +2,23 @@ import { ThemeMode } from "@prisma/client";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-type Store = {
+type State = {
   organizationId: string | undefined;
   mode: ThemeMode;
-  actions: Actions;
 };
 type Actions = {
-  updateOrganizationId: (id: Store["organizationId"]) => void;
-  updateMode: (mode: Store["mode"]) => void;
+  updateOrganizationId: (id: State["organizationId"]) => void;
+  updateMode: (mode: State["mode"]) => void;
 };
+type Store = State & Actions;
 
 const useConfig = create<Store, [["zustand/persist", unknown]]>(
   persist(
     (set) => ({
       mode: ThemeMode.SYSTEM,
       organizationId: undefined,
-      actions: {
-        updateMode: (mode) => set({ mode }),
-        updateOrganizationId: (id) => set({ organizationId: id }),
-      },
+      updateMode: (mode) => set({ mode }),
+      updateOrganizationId: (id) => set({ organizationId: id }),
     }),
     {
       name: "config-storage",
@@ -31,4 +29,8 @@ const useConfig = create<Store, [["zustand/persist", unknown]]>(
 export const useOrganizationId = () =>
   useConfig((state) => state.organizationId);
 export const useThemeMode = () => useConfig((state) => state.mode);
-export const useConfigActions = () => useConfig((state) => state.actions);
+export const useConfigActions = () =>
+  useConfig((state) => ({
+    updateOrganizationId: state.updateOrganizationId,
+    updateMode: state.updateMode,
+  }));
