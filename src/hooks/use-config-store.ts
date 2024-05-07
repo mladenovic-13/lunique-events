2b52d90bb69b06d44704b0d772fc341d649896fa
Nikola Mladenović <nikolanik999@gmail.com>
2024-05-07 17:12:17 +0200

    fix: rm theme

--- src/hooks/use-config-store.ts
+++ src/hooks/use-config-store.ts
@@ -1,23 +1,18 @@
-import { ThemeMode } from "@prisma/client";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 type State = {
   organizationId: string | undefined;
-  mode: ThemeMode;
 };
 type Actions = {
   updateOrganizationId: (id: State["organizationId"]) => void;
-  updateMode: (mode: State["mode"]) => void;
 };
 type Store = State & Actions;
 
 const useConfig = create<Store, [["zustand/persist", unknown]]>(
   persist(
     (set) => ({
-      mode: ThemeMode.SYSTEM,
       organizationId: undefined,
-      updateMode: (mode) => set({ mode }),
       updateOrganizationId: (id) => set({ organizationId: id }),
     }),
     {
@@ -28,9 +23,7 @@ const useConfig = create<Store, [["zustand/persist", unknown]]>(
 
 export const useOrganizationId = () =>
   useConfig((state) => state.organizationId);
-export const useThemeMode = () => useConfig((state) => state.mode);
 export const useConfigActions = () =>
   useConfig((state) => ({
     updateOrganizationId: state.updateOrganizationId,
-    updateMode: state.updateMode,
   }));
