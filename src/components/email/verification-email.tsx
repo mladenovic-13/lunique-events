52ab79a48c996ae4f283a83d5e8ad812e2792e47
Lukiano99 <l.stojadinovic99@gmail.com>
2024-05-16 19:15:01 +0200

    feat: redesign verification mail

--- src/components/email/verification-email.tsx
+++ src/components/email/verification-email.tsx
@@ -1,87 +1,71 @@
 import { Body } from "@react-email/body";
+import { Button, Img } from "@react-email/components";
 import { Container } from "@react-email/container";
 import { Head } from "@react-email/head";
 import { Heading } from "@react-email/heading";
 import { Hr } from "@react-email/hr";
 import { Html } from "@react-email/html";
-import { Link } from "@react-email/link";
 import { Preview } from "@react-email/preview";
 import { Section } from "@react-email/section";
+import { Tailwind } from "@react-email/tailwind";
 import { Text } from "@react-email/text";
 
 interface VerificationEmail {
   magicLink: string;
+  logo: string;
 }
 
-export const VerificationEmail = ({ magicLink }: VerificationEmail) => (
+export const VerificationEmail = ({ magicLink, logo }: VerificationEmail) => (
   <Html>
     <Head />
-    <Preview>Log in with this magic link.</Preview>
-    <Body style={main}>
-      <Container style={container}>
-        <Heading style={heading}>🪄 Your magic link</Heading>
-        <Section style={body}>
-          <Text style={paragraph}>
-            <Link style={link} href={magicLink}>
-              👉 Click here to sign in 👈
-            </Link>
+    <Preview>Your Access to Lunique Events is Ready!</Preview>
+    <Tailwind>
+      <Body className="m-auto bg-white px-2 font-sans">
+        <Container className="mx-auto my-[40px] max-w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
+          <Section className="mt-[32px]">
+            <Img
+              src={logo}
+              alt="Lunique Events"
+              width="40"
+              height="37"
+              className="object-cover"
+            />
+          </Section>
+          <Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
+            <strong>We’re Excited You’re Here! </strong>
+          </Heading>
+          <Text className="text-[14px] leading-[24px] text-black">
+            Hello,
+            <strong>Lunique Events</strong> is ready for You!
           </Text>
-          <Text style={paragraph}>
-            If you didn&apos;t request this, please ignore this email.
+          <Text className="text-[14px] leading-[24px] text-black">
+            Create your next unforgettable event now.
           </Text>
-        </Section>
-        <Text style={paragraph}>
-          Best,
-          <br />- Lunique Tech Team
-        </Text>
-        <Hr style={hr} />
-        <Text style={footer}>Lunique Tech Inc.</Text>
-        <Text style={footer}>Sutjeska 40, Pirot, Srbija</Text>
-      </Container>
-    </Body>
+          <Text className="text-[14px] leading-[24px] text-black">
+            By clicking this button, you will be redirected to your account
+            page.
+          </Text>
+          <Section className="my-[32px]  text-center">
+            <Button
+              className="rounded bg-[#000000] px-5 py-3 text-center text-[12px] font-semibold text-white no-underline"
+              href={magicLink}
+            >
+              Sign In
+            </Button>
+          </Section>
+
+          <Hr className="mx-0 my-[26px] w-full border border-solid border-[#eaeaea]" />
+          <Text className="text-[12px] leading-[24px] text-[#666666]">
+            This link will only be valid for the next 5 minutes.
+            <br />
+            If you {"didn't"} try to login, you can safely ignore this email.
+          </Text>
+          <Text className="text-[12px] leading-[24px] text-[#666666]">
+            Best, <br />
+            <strong>Lunique Team</strong>
+          </Text>
+        </Container>
+      </Body>
+    </Tailwind>
   </Html>
 );
-
-const main = {
-  backgroundColor: "#ffffff",
-  fontFamily:
-    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
-};
-
-const container = {
-  margin: "0 auto",
-  padding: "20px 25px 48px",
-  backgroundImage: 'url("/assets/raycast-bg.png")',
-  backgroundPosition: "bottom",
-  backgroundRepeat: "no-repeat, no-repeat",
-};
-
-const heading = {
-  fontSize: "28px",
-  fontWeight: "bold",
-  marginTop: "48px",
-};
-
-const body = {
-  margin: "24px 0",
-};
-
-const paragraph = {
-  fontSize: "16px",
-  lineHeight: "26px",
-};
-
-const link = {
-  color: "#FF6363",
-};
-
-const hr = {
-  borderColor: "#dddddd",
-  marginTop: "48px",
-};
-
-const footer = {
-  color: "#8898aa",
-  fontSize: "12px",
-  marginLeft: "4px",
-};
--- src/server/send-verification-email.ts
+++ src/server/send-verification-email.ts
@@ -5,6 +5,7 @@ import { VerificationEmail } from "@/components/email/verification-email";
 import { env } from "@/env.mjs";
 
 const resend = new Resend(env.EMAIL_SERVER_PASSWORD);
+const logo = `${env.NEXT_PUBLIC_AWS_CLOUDFRONT_DOMAIN}/assets/logo.png`;
 
 export async function sendVerificationRequest({
   identifier,
@@ -15,6 +16,6 @@ export async function sendVerificationRequest({
     from: `Lunique Tech <${provider.from}>`,
     to: [identifier],
     subject: "Sign In Magic Link",
-    react: VerificationEmail({ magicLink: url }),
+    react: VerificationEmail({ magicLink: url, logo: logo }),
   });
 }
