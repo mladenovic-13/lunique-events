13dbc9f0cbc61d7a0318883d7badb875c2bf7a04
Nikola Mladenović <nikolanik999@gmail.com>
2024-05-30 14:21:51 +0200

    chore: query infinite stale time

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -9,7 +9,14 @@ import SuperJSON from "superjson";
 
 import { type AppRouter } from "@/server/api/root";
 
-const createQueryClient = () => new QueryClient();
+const createQueryClient = () =>
+  new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: Infinity,
+      },
+    },
+  });
 
 let clientQueryClientSingleton: QueryClient | undefined = undefined;
 const getQueryClient = () => {
