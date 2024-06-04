1223b28fd45d268feb5d6fa616c499440d527493
Luka Stojadinovic <80914501+Lukiano99@users.noreply.github.com>
2024-06-04 18:13:00 +0200

    feat: landing page (#152)
    * feat: wip - landing page
    
    * feat: wip
    
    * feat: events carousel
    
    * feat: carousel wip
    
    * feat: carousel update
    
    * feat: header
    
    * feat: carousel skeleton

---
+++ src/app/(landing)/_components/carousel.tsx
@@ -0,0 +1,79 @@
+import * as React from "react";
+import { type Event } from "@prisma/client";
+import Autoplay from "embla-carousel-autoplay";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import { api } from "@/trpc/react";
+
+import EventCardCarousel from "./event-card-carousel";
+
+export function CarouselEvents() {
+  const plugin = React.useRef(
+    Autoplay({
+      delay: 0,
+      stopOnMouseEnter: true,
+      stopOnFocusIn: false,
+      stopOnInteraction: false,
+    }),
+  );
+
+  const { data, isLoading, fetchNextPage, isFetchingNextPage, hasNextPage } =
+    api.explore.list.useInfiniteQuery(
+      {
+        limit: 12,
+      },
+      {
+        getNextPageParam: (lastPage) => lastPage.nextCursor,
+      },
+    );
+
+  const events = data?.pages
+    .map((page) => page.events)
+    .reduce((acc: Event[], events) => [...acc, ...events], []);
+
+  return (
+    <Carousel
+      plugins={[plugin.current]}
+      className="flex size-full items-start justify-center rounded-lg md:w-[1280px]"
+      opts={{
+        align: "start",
+        loop: true,
+        duration: 15000,
+      }}
+    >
+      <div className="absolute -top-10 left-0 z-10 -ml-10 flex h-[120%] w-[80px] items-center justify-end bg-gradient-to-r from-background via-background to-background/95 blur-lg md:w-[150px]"></div>
+      <CarouselContent className="w-full">
+        {isLoading &&
+          Array.from({ length: 6 }).map((_, index) => (
+            <CarouselItem
+              key={index}
+              className="items-center justify-center px-1  md:basis-1/4"
+            >
+              <div className="py-1">
+                <EventCardCarousel key={index} />
+              </div>
+            </CarouselItem>
+          ))}
+        {!isLoading &&
+          events?.map((event, index) => (
+            <CarouselItem
+              key={index}
+              className="items-center justify-center px-1  md:basis-1/4"
+            >
+              <div className="py-1">
+                <EventCardCarousel {...event} key={index} />
+              </div>
+            </CarouselItem>
+          ))}
+      </CarouselContent>
+      <div className="absolute -top-10 right-0 z-10 -mr-10 flex h-[120%] w-[80px] items-center justify-end bg-gradient-to-l from-background via-background to-background/95 blur-lg md:w-[150px]"></div>
+
+      {/* <CarouselPrevious /> */}
+      {/* <CarouselNext className="hidden" /> */}
+    </Carousel>
+  );
+}
---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "next/link";
+
+import { CarouselEvents } from "./_components/carousel";
+import { Heading } from "./_components/heading";
+
+export default function Home() {
+  return (
+    <div className="flex w-full flex-col items-center justify-center">
+      <div className="flex w-full flex-col items-center justify-start pt-10  md:gap-4 md:px-0 md:pt-12 ">
+        <Link
+          className="w-fit animate-pulse rounded-lg bg-muted px-4 py-1 text-sm  drop-shadow-md transition-all md:text-base md:hover:bg-accent-foreground/20"
+          href={"https://lunique.tech"}
+          target="_blank"
+        >
+          <p>
+            ✨ build by <strong>Lunique</strong>
+          </p>
+        </Link>
+        <Heading />
+        <div className="flex w-full items-center justify-center pt-10 md:pt-10">
+          <CarouselEvents />
+        </div>
+      </div>
+    </div>
+  );
+}
