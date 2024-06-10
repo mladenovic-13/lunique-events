af4f8a7b2e4628cd8e13127a1321cec95254d3f5
Lukiano99 <l.stojadinovic99@gmail.com>
2024-06-10 19:30:35 +0200

    feat: ladning wip

--- src/app/(landing)/_components/carousel-events.tsx
+++ src/app/(landing)/_components/carousel-events.tsx
@@ -1,68 +1,16 @@
 import * as React from "react";
-import Autoplay from "embla-carousel-autoplay";
-import events from "events";
 
 import Marquee from "@/components/magicui/marquee";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
 import { api } from "@/trpc/react";
 
 import EventCardCarousel from "./event-card-carousel";
 
 export function CarouselEvents() {
-  const plugin = React.useRef(
-    Autoplay({
-      delay: 0,
-      stopOnMouseEnter: true,
-      stopOnFocusIn: false,
-      stopOnInteraction: false,
-    }),
-  );
-
-  const { data: events, isLoading } = api.explore.featured.useQuery({
+  const { data: events } = api.explore.featured.useQuery({
     limit: 10,
   });
   if (events) console.log(events[0]?.thumbnailUrl);
   return (
-    // <Carousel
-    //   plugins={[plugin.current]}
-    //   className="flex size-full items-start justify-center rounded-lg md:w-[1280px]"
-    //   opts={{
-    //     align: "start",
-    //     loop: true,
-    //     duration: 15000,
-    //   }}
-    // >
-    //   <CarouselContent className="w-full">
-    //     {isLoading &&
-    //       Array.from({ length: 6 }).map((_, index) => (
-    //         <CarouselItem
-    //           key={index}
-    //           className="basis-[70%] items-center justify-center px-1  md:basis-1/4"
-    //         >
-    //           <div className="py-1">
-    //             <EventCardCarousel key={index} />
-    //           </div>
-    //         </CarouselItem>
-    //       ))}
-    //     {!isLoading &&
-    //       events?.map((event, index) => (
-    //         <CarouselItem
-    //           key={index}
-    //           className="basis-[70%] items-center justify-center px-1  md:basis-1/4"
-    //         >
-    //           <div className="py-1">
-    //             <EventCardCarousel {...event} key={index} />
-    //           </div>
-    //         </CarouselItem>
-    //       ))}
-    //   </CarouselContent>
-    //   <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
-    //   <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
-    // </Carousel>
     <div className="flex size-full">
       <div className="relative hidden flex-row items-center justify-center overflow-hidden rounded-lg bg-background sm:px-20 md:flex md:h-[700px]  lg:h-[800px]">
         <Marquee pauseOnHover vertical className="[--duration:90s]">
--- src/app/(landing)/_components/onboarding-steps.tsx
+++ src/app/(landing)/_components/onboarding-steps.tsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { useTheme } from "next-themes";
 
-import createAccountImg from "@/public/images/landing-page-images/create-account.webp";
-import createEventImg from "@/public/images/landing-page-images/create-event.webp";
-import eventLandingImg from "@/public/images/landing-page-images/event-landing-page.webp";
-import inviteGuestsImg from "@/public/images/landing-page-images/invite-guests.webp";
+import createAccountDarkImg from "@/public/images/landing-page-images/create-account-dark.webp";
+import createAccountLightImg from "@/public/images/landing-page-images/create-account-light.webp";
+import createEventDarkImg from "@/public/images/landing-page-images/create-event-dark.webp";
+import createEventLightImg from "@/public/images/landing-page-images/create-event-light.webp";
+import inviteGuestsDarkImg from "@/public/images/landing-page-images/invite-guests-dark.webp";
+import inviteGuestsLightImg from "@/public/images/landing-page-images/invite-guests-light.webp";
 
 import StepCard from "./step-card";
-
 interface OnboardingStepsProps {
   props?: string;
 }
 const OnboardingSteps = ({}: OnboardingStepsProps) => {
+  const { theme } = useTheme();
   return (
-    <section className="flex flex-col items-center justify-center gap-6 px-4 pt-10 md:w-[1280px] md:px-0 md:pt-0">
+    <section className="flex w-full max-w-screen-xl flex-col items-center justify-center gap-6 px-4 pt-10 md:px-0 md:pt-0">
       <h1 className="text-3xl font-semibold md:text-5xl">
         How It{" "}
         <strong className="bg-gradient-to-r from-rose-800 to-red-500 bg-clip-text font-bold capitalize text-transparent">
@@ -23,80 +26,117 @@ const OnboardingSteps = ({}: OnboardingStepsProps) => {
         Follow these steps to create your first event that everyone will love.
         Our intuitive platform makes event planning simple and enjoyable.
       </p>
-      {/* <div className="flex size-full flex-col gap-4">
+      <div className="flex w-full flex-col items-center justify-center space-y-4 md:flex-row md:space-y-0">
         <StepCard
-          title="Create Your Account ✨"
-          description="Sign up quickly and easily using just your email address. You will
-              receive a Magic Link that will create your profile and log you
-              into the platform with just one click."
+          title="Create Your Account"
+          description="Easily and without any prerequisites!"
           step={1}
-          image={createAccountImg.src}
-          // className="flex-col gap-12 md:flex-row md:gap-0"
-          className=""
+          image={
+            theme === "light"
+              ? createAccountLightImg.src
+              : theme === "dark"
+                ? createAccountDarkImg.src
+                : createAccountLightImg.src
+          }
         />
-        <div className="flex h-fit flex-col items-start justify-center gap-4 md:flex-row">
-          <StepCard
-            title="Create Your First Event 🥳"
-            description="After registration, users can immediately start creating their first event. They can customize every detail, from the event name to the date and time."
-            step={2}
-            className="flex flex-col"
-            image={createEventImg.src}
-          />
-          <StepCard
-            title="Personalize Your Event Page 🪄"
-            description="Users can customize the look and feel of their event page, add images, descriptions, and personalized messages for guests."
-            step={3}
-            className="flex flex-col"
-            image={eventLandingImg.src}
-          />
+        <div className="hidden w-32 p-0 md:flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            viewBox="0 0 800 800"
+          >
+            <g
+              stroke-width="19"
+              stroke="hsl(347, 77%, 50%)"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              transform="matrix(0.7313537016191707,-0.6819983600624983,0.6819983600624983,0.7313537016191707,-184.34082467266762,382.25786337733103)"
+            >
+              <path
+                d="M221.92139434814453 169Q-129.07860565185547 271 683.9213943481445 631 "
+                marker-end="url(#SvgjsMarker2236)"
+              ></path>
+            </g>
+            <defs>
+              <marker
+                markerWidth="4"
+                markerHeight="4"
+                refX="2"
+                refY="2"
+                viewBox="0 0 4 4"
+                orient="auto"
+                id="SvgjsMarker2236"
+              >
+                <polygon
+                  points="0,4 0,0 4,2"
+                  fill="hsl(347, 77%, 50%)"
+                ></polygon>
+              </marker>
+            </defs>
+          </svg>
         </div>
         <StepCard
-          title="Invite Your Guests 📨"
-          description="Invite guests quickly and easily via email, social media, or by sharing a unique link. Keep track of who has confirmed attendance and who is still considering, so you're always up to date with the number of attendees."
-          step={4}
-          className="flex-col gap-12 md:flex-row-reverse md:gap-0"
-          image={inviteGuestsImg.src}
+          title="Create Your First Event"
+          description="Easily and without any prerequisites!"
+          step={2}
+          image={
+            theme === "light"
+              ? createEventLightImg.src
+              : theme === "dark"
+                ? createEventDarkImg.src
+                : createEventLightImg.src
+          }
         />
-      </div> */}
-      <div className="flex w-full flex-col space-y-3">
-        <div className="flex w-full flex-col items-center justify-between space-y-3 md:flex-row md:space-y-0">
-          <StepCard
-            title="Create Your Account ✨"
-            description="Sign up quickly and easily using just your email address. You will
-              receive a Magic Link that will create your profile and log you
-              into the platform with just one click."
-            step={1}
-            image={createAccountImg.src}
-          />
-          <StepCard
-            title="Create Your First Event 🥳"
-            description="After registration, users can immediately start creating their first event. They can customize every detail, from the event name to the date and time."
-            step={2}
-            image={createEventImg.src}
-          />
-          <StepCard
-            title="Personalize Your Event Page 🪄"
-            description="Users can customize the look and feel of their event page, add images, descriptions, and personalized messages for guests."
-            step={3}
-            image={eventLandingImg.src}
-          />
+        <div className="hidden w-32 p-0 md:flex">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            viewBox="0 0 800 800"
+          >
+            <g
+              stroke-width="19"
+              stroke="hsl(347, 77%, 50%)"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              transform="matrix(0.7313537016191707,-0.6819983600624983,0.6819983600624983,0.7313537016191707,-184.34082467266762,382.25786337733103)"
+            >
+              <path
+                d="M167.4549560546875 169Q670.4549560546875 271 629.4549560546875 631 "
+                marker-end="url(#SvgjsMarker2447)"
+              ></path>
+            </g>
+            <defs>
+              <marker
+                markerWidth="4"
+                markerHeight="4"
+                refX="2"
+                refY="2"
+                viewBox="0 0 4 4"
+                orient="auto"
+                id="SvgjsMarker2447"
+              >
+                <polygon
+                  points="0,4 0,0 4,2"
+                  fill="hsl(347, 77%, 50%)"
+                ></polygon>
+              </marker>
+            </defs>
+          </svg>
         </div>
-        <div className="flex w-full flex-col items-center justify-between md:flex-row">
-          <StepCard
-            title="Invite Your Guests 📨"
-            description="Invite guests quickly and easily via email, social media, or by sharing a unique link. Keep track of who has confirmed attendance and who is still considering, so you're always up to date with the number of attendees."
-            step={4}
-            image={inviteGuestsImg.src}
-            className="w-full"
-          />
-          <StepCard
-            title="Invite Your Guests 📨"
-            description="Invite guests quickly and easily via email, social media, or by sharing a unique link. Keep track of who has confirmed attendance and who is still considering, so you're always up to date with the number of attendees."
-            step={4}
-            image={inviteGuestsImg.src}
-            className="w-full flex-1"
-          />
-        </div>
+        <StepCard
+          title="Invite Your Guests. Now!"
+          description="Easily and without any prerequisites!"
+          step={2}
+          image={
+            theme === "light"
+              ? inviteGuestsLightImg.src
+              : theme === "dark"
+                ? inviteGuestsDarkImg.src
+                : inviteGuestsLightImg.src
+          }
+        />
       </div>
     </section>
   );
