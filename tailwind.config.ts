ab4c308022d062f1a8ba09850abcdf841b067d0b
Lukiano99 <80914501+Lukiano99@users.noreply.github.com>
2024-06-12 20:21:42 +0200

    feat: signin page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,6 +84,14 @@ module.exports = {
           from: { transform: "translateY(0)" },
           to: { transform: "translateY(calc(-100% - var(--gap)))" },
         },
+        ripple: {
+          "0%, 100%": {
+            transform: "translate(-50%, -50%) scale(1)",
+          },
+          "50%": {
+            transform: "translate(-50%, -50%) scale(0.9)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
@@ -92,6 +100,7 @@ module.exports = {
         "collapsible-up": "collapsible-up 0.2s ease-out",
         marquee: "marquee var(--duration) linear infinite",
         "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+        ripple: "ripple 3400ms ease infinite",
       },
     },
   },
